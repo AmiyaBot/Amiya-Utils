4473a0a5b61b1656f127a5a49ad1525cbc803fa2
Vivien <826197021@qq.com>
2025-07-09 12:16:31 +0800

    add ci

--- setup.py
+++ setup.py
@@ -1,4 +1,85 @@
+import json
+import random
 import setuptools
+
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+from urllib import request
+
+
+def ver_num(_v):
+    num = int(_v.replace('.', ''))
+    if num < 1000:
+        num *= 10
+    return num
+
+
+def get_new_version():
+    pypi = json.loads(request.urlopen('https://pypi.python.org/pypi/amiyautils/json').read())
+    v_list = {ver_num(v): v for v in pypi['releases'].keys()}
+    s_list = sorted(v_list)
+    latest = v_list[s_list[-1]]
+
+    print(f'latest: {latest}')
+
+    return f'{latest}'
+
+
+# Auto increment the version number.
+# 1.0.9 -> 1.1.0 , 1.9.9 -> 2.0.0 but 9.9.9 -> 10.0.0
+def incr_version(v):
+    v = v.split('.')
+    if len(v) == 3:
+        if int(v[2]) >= 9:
+            v[2] = '0'
+            if int(v[1]) >= 9:
+                v[1] = '0'
+                v[0] = str(int(v[0]) + 1)
+            else:
+                v[1] = str(int(v[1]) + 1)
+        else:
+            v[2] = str(int(v[2]) + 1)
+    else:
+        v.append('1')
+    return '.'.join(v)
+
+
+class CustomBdistWheelCommand(_bdist_wheel):
+    user_options = _bdist_wheel.user_options + [
+        (
+            'auto-increment-version',
+            None,
+            'Auto increment the version number before building with special rule: 1.0.9 -> 1.1.0 , 1.9.9 -> 2.0.0 . However 9.9.9 -> 10.0.0',
+        )
+    ]
+
+    def initialize_options(self):
+        _bdist_wheel.initialize_options(self)
+        self.auto_increment_version = False
+
+    def finalize_options(self):
+        latest_version = get_new_version()
+        if self.auto_increment_version:
+            new_version = incr_version(latest_version)
+            print(f'Auto-incrementing version to: {new_version}')
+            self.distribution.metadata.version = new_version
+        else:
+            new_version = incr_version(latest_version)
+            release_new = input(f'new?: {new_version} (Y/n)')
+
+            if not (not release_new or release_new.lower() == 'y'):
+                new_version = input('version: ')
+
+        self.distribution.metadata.version = new_version
+
+        # 加入一个随机数的BuildNumber保证Action可以重复执行
+        build_number = random.randint(0, 1000)
+        self.build_number = f'{build_number}'
+
+        _bdist_wheel.finalize_options(self)
+
+    def run(self):
+        _bdist_wheel.run(self)
+
 
 with open('README.md', mode='r', encoding='utf-8') as md:
     description = md.read()
@@ -23,6 +104,9 @@ setuptools.setup(
     include_package_data=True,
     python_requires='>=3.10',
     install_requires=requirements,
+    cmdclass={
+        'bdist_wheel': CustomBdistWheelCommand,
+    },
 )
 
 # python setup.py bdist_wheel
